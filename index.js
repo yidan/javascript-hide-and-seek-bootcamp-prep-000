9736503aa4db4e00d08ff16c954218269584ac8c
Yidan Wang <wangyidan621@gmail.com>
2017-12-16 18:41:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  let e = document.querySelectorAll(".ranked-list li") 
+  let e = document.querySelectorAll(".ranked-list li")
   e.map(x => x.innerHTML = parseInt(x.innerHTML,10)+n)
   
 }
-
-
