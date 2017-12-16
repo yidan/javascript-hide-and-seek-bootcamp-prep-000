64bd27916c43f630394c5567d390935a8bbe0fa9
Yidan Wang <wangyidan621@gmail.com>
2017-12-16 18:36:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector("#nested .target")
 }
+
+function increaseRankBy(n){
+  let e = document.querySelector(".ranked-list")
+  e.innerHTML = parseInt(e.innerHTML,10)+n
+}
