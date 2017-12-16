fa9b52a6ccd1b6902512973fa0515c91316f9db0
Yidan Wang <wangyidan621@gmail.com>
2017-12-16 18:55:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n){
+  let e = document.querySelectorAll(".ranked-list li")
+  Array.prototype.map.call(e,x => x.innerHTML = parseInt(x.innerHTML,10)+n)
+
+}
+
+function deepestChild(){
+  return document.querySelector("div#grand-node div div div div")
+}
